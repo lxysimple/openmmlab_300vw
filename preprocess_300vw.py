3d9bee50ea626bcc207ff80efdf7732105778fcb
lxysimple <128936873+lxysimple@users.noreply.github.com>
2024-01-25 19:35:10 +0800

    scale = max(w, h) + 60

--- preprocess_300vw.py
+++ preprocess_300vw.py
@@ -221,7 +221,7 @@ class Preprocess300vw:
 
                     annotation['bbox'] = [x_left, y_high, x_right, y_low]
 
-                    scale = max(w, h) + 20
+                    scale = max(w, h) + 60 # 300w should be about 60
                     scale = scale / 200.0
                     annotation['scale'] = scale
 
